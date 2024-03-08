3ec618870f4fcf9c4386c0d21eb599fe791ded28
Amir <amirnikravan@outlook.com>
2024-03-08 21:18:43 +0330

    start project

---
+++ build-DoorNegar-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_DoorNegar_FILE D:/uniproject/build-DoorNegar-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/DoorNegar.exe)
+set(__QT_DEPLOY_TARGET_DoorNegar_RUNTIME_DLLS D:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
